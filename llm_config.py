d55d5ff74704da0c16d52c3d35d744f3594a01dd
yaraeslamm <yaraelshehawi@gmail.com>
2026-02-16 19:37:04 +0200

    changing llama to groq

--- llm_config.py
+++ llm_config.py
@@ -1,3 +1,16 @@
-from langchain_community.llms import Ollama
+# from langchain_community.llms import Ollama
 
-llm = Ollama(model="llama3")
+# llm = Ollama(model="llama3")
+
+from langchain_community.chat_models import ChatGroq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+GROQ_API_KEY = os.getenv("GROQ_API_KEY") 
+
+llm = ChatGroq(
+    temperature=0.3,
+    model_name="llama3-70b-8192",
+    groq_api_key=GROQ_API_KEY
+)
